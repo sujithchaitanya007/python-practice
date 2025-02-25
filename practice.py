f9a40493277a0bdbe44139686ea8f8edd3821abf
Sujith Chaitanya Padavala <123373761+sujithchaitanya007@users.noreply.github.com>
2025-02-25 15:37:00 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,17 @@
+sujith = 7
+Sujith = 23
+print(sujith)
+print(Sujith)
+Sujith = [1,2,3,45,23,7]
+print(Sujith)
+print(Sujith[2])
+print(Sujith[-2:-1])
+Sujith.append(77)
+print(Sujith)
+Sujith.remove(2)
+print(Sujith)
+Sujith.insert(1,2)
+print(Sujith)
+sujith = (7,23,4,5,8)
+print(sujith)
+print(sujith[0:2])
